c5e773de51c3688fd1cc8fa54f1280928f50049e
Divya Koyyalagunta <divyakoyy@gmail.com>
2023-09-05 18:08:00 -0400

    update scripts to use metient api

--- src/test/machina_simulated_data/predict_single_simulated_tree.sh
+++ src/test/machina_simulated_data/predict_single_simulated_tree.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ ! $# -eq 14 ]
+if [ ! $# -eq 15 ]
 then
-    echo "Usage: $0 <machina_sims_data_dir> <num_sites> <mig_type> <seed> <data_fit_weight> <mig_weight> <comig_weight> <seed_weight> <reg_weight> <gen_dist_weight> <weight_init_primary> <batch_size> <lr_sched> <out_dir>"
+    echo "Usage: $0 <machina_sims_data_dir> <num_sites> <mig_type> <seed> <tree_num> <data_fit_weight> <mig_weight> <comig_weight> <seed_weight> <reg_weight> <gen_dist_weight> <weight_init_primary> <batch_size> <lr_sched> <out_dir>"
     exit 1
 fi
 
@@ -11,4 +11,4 @@ fi
 
 source activate met
 
-python predict_single_simulated_tree.py ${1} --num_sites ${2} --mig_type ${3} --seed ${4} --wdata_fit ${5} --wmig ${6} --wcomig ${7} --wseed ${8} --wreg ${9} --wgen ${10}  --wip ${11} --bs ${12} --lr_sched ${13} --out_dir ${14}
+python predict_single_simulated_tree.py ${1} --num_sites ${2} --mig_type ${3} --seed ${4} --tree_num ${5} --wdata_fit ${6} --wmig ${7} --wcomig ${8} --wseed ${9} --wreg ${10} --wgen ${11}  --wip ${12} --bs ${13} --lr_sched ${14} --out_dir ${15}
