22629dae86426f285019832515c500689524889f
Divya Koyyalagunta <divyakoyy@gmail.com>
2023-12-13 14:51:36 -0500

    clenaup old files under src

---
+++ metient/data/tracerx_nsclc/metient_outputs/run_metient_orchard_patient.py
@@ -0,0 +1,80 @@
+import sys
+import os
+import argparse
+import seaborn as sns
+import pandas as pd
+import torch
+import matplotlib.pyplot as plt
+from src.lib.metient import *
+from src.util import pairtree_data_extraction_util as pt_util
+
+custom_colors = ["#6aa84fff","#c27ba0ff", "#e69138ff", "#be5742e1", "#2496c8ff", "#674ea7ff"] + sns.color_palette("Paired").as_hex()
+
+def find_labeling(ref_var_fn, tree, custom_colors, primary_site, patient_name, output_dir, weights):    
+    ref_matrix, var_matrix, unique_sites, idx_to_full_cluster_label = get_ref_var_matrices(ref_var_fn)
+
+    idx_to_cluster_label = dict()
+    for ix in idx_to_full_cluster_label:
+        og_label_muts = idx_to_full_cluster_label[ix].split(';') # e.g. CUL3:2:225371655:T;TRPM6:9:77431650:C
+        if len(og_label_muts) > 3:
+            og_label_muts = og_label_muts[:3]
+        gene_names = []
+        for mut_label in og_label_muts:
+            gene_names.append(mut_label.split(":")[0])
+        idx_to_cluster_label[ix] = ("_").join(gene_names)
+    print(idx_to_cluster_label)
+    print(tree.shape)
+
+    print(f"Anatomical sites: {unique_sites}")   
+    G = get_genetic_distance_matrix_from_adj_matrix(tree,idx_to_full_cluster_label, ";")
+    print_config = PrintConfig(visualize=True, verbose=False, viz_intermeds=False, k_best_trees=10)
+    get_migration_history(tree, ref_matrix, var_matrix, unique_sites, primary_site, idx_to_cluster_label,
+                          weights, print_config, output_dir, patient_name, G=G, 
+                          weight_init_primary=True, custom_colors=custom_colors, 
+                          batch_size=6000, max_iter=100)
+
+    
+def run_orchard_patient(patient, weights, tsv_dir, tree_dir, output_dir):
+    space = "x"*44
+    tsv_fn = os.path.join(tsv_dir, f"{patient}_clustered_SNVs.tsv")
+    print(f"{space} PATIENT {patient} {space}")
+    df = pd.read_csv(tsv_fn, delimiter="\t")
+    primary_sites = list(df[df['sample_type']=='primary']['anatomical_site_label'].unique())
+    if (len(primary_sites) > 1):
+        print("*Multiple primary samples, running metient once for each possible primary*")
+
+    for primary_site in primary_sites:
+        print(f"Primary site: {primary_site}")
+        run_name = f"{patient}_{primary_site}"
+        tree_fn = os.path.join(tree_dir, f"{patient}_pyclone_clustered.results.npz")
+        data = pt_util.get_adj_matrices_from_pairtree_results(tree_fn)
+        tree, _ = data[0] # Use best tree
+        find_labeling(tsv_fn, tree, custom_colors, primary_site, run_name, output_dir, weights)
+
+
+
+if __name__=="__main__":
+    
+    parser = argparse.ArgumentParser(description='run metient on conipher generated trees (with and without genetic distance).')
+    parser.add_argument('patient', type=str,
+                        help='an integer for the accumulator')
+    parser.add_argument('tsv_dir', type=str,
+                        help='directory with clustered tsvs')
+    parser.add_argument('tree_dir', type=str,
+                        help='directory with conipher trees')
+    parser.add_argument('output_dir', type=str,
+                        help='parent output directory, where subdirectories for each configuration will get created')
+    args = parser.parse_args()
+    patient = args.patient
+
+    # (1) Maximum parsimony
+    weights = Weights(data_fit=0.2, mig=10.0, comig=7.0, seed_site=5.0, reg=2.0, gen_dist=0.0)
+    output_dir = os.path.join(args.output_dir, "max_pars")
+    run_orchard_patient(patient, weights, args.tsv_dir, args.tree_dir, output_dir)
+
+    # (2) Maximum parsimony + genetic distance
+    weights = Weights(data_fit=0.2, mig=10.0, comig=7.0, seed_site=5.0, reg=2.0, gen_dist=1.0)
+    output_dir = os.path.join(args.output_dir, "max_pars_genetic_distance")
+    run_orchard_patient(patient, weights, args.tsv_dir, args.tree_dir, output_dir)
+
+    
--- src/test/machina_simulated_data/README.md
+++
@@ -1,8 +0,0 @@
-python predict_all_simulated_trees.py ../../data/machina_sims/ bs_256_gd_r1_wip_10032023 --bs 256 --gen 1.0 --wip; 
-to run machina:
-
-export GRB_LICENSE_KEY="/home/koyyald/gurobi.lic"
-export GRB_LICENSE_FILE="/home/koyyald/gurobi.lic"
-export LD_LIBRARY_PATH=/lila/home/koyyald/mambaforge/envs/machina/lib/libstdc++.so.6:$LD_LIBRARY_PATH
-export LD_PRELOAD=/lila/home/koyyald/mambaforge/envs/machina/lib/libstdc++.so.6
-./machina_pmh_ti_timing.sh /data/morrisq/divyak/machina-linux-binaries/pmh_ti ../../../../machina/data/sims/ ../../data/machina_sims/
